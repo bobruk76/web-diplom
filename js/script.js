1fbcc31d7261b230bde90085f61e5a00efcf8315
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-05-31 14:25:12 +0300

    fix acordion

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ const accordion = new Accordion('.guests__accordion', {
   elementClass: 'guests__accordion-item',
   panelClass: 'guests__items',
   triggerClass: 'guests__accordion-item-title',
+  openOnInit: [0],
   beforeOpen: (currentElement) => {
     const plusButton = currentElement.querySelector('.arrow-in-circle')
     plusButton.classList.add('arrow-in-circle--active')
