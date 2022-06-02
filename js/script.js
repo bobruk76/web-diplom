91a97b0f40bc38461e8af97b9d9165e69f8e5566
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-06-02 13:14:17 +0300

    add validator to input fields

--- js/script.js
+++ js/script.js
@@ -1,58 +1,91 @@
 const elementSelect = document.querySelector('.select');
 const choices = new Choices(elementSelect, {
-  itemSelectText: '',
-  placeholder: false,
-  choices: [
+    itemSelectText: '',
+    placeholder: false,
+    choices: [
 
-    {
-      value: 'Дмитрий Гутенберг',
-      label: 'Дмитрий Гутенберг',
-      selected: true,
-      disabled: false,
-    },
-    {
-      value: 'Татьяна Флеганова',
-      label: 'Татьяна Флеганова',
-      selected: false,
-      disabled: false,
-    },
-    {
-      value: 'Анна Васильева',
-      label: 'Анна Васильева',
-      selected: false,
-      disabled: false,
-    },
-    {
-      value: 'Пётр Дмитриевский',
-      label: 'Пётр Дмитриевский',
-      selected: false,
-      disabled: false,
-    },
-  ],
-  searchEnabled: false,
-  shouldSort: false,
-  placeholder: false,
+        {
+            value: 'Дмитрий Гутенберг',
+            label: 'Дмитрий Гутенберг',
+            selected: true,
+            disabled: false,
+        },
+        {
+            value: 'Татьяна Флеганова',
+            label: 'Татьяна Флеганова',
+            selected: false,
+            disabled: false,
+        },
+        {
+            value: 'Анна Васильева',
+            label: 'Анна Васильева',
+            selected: false,
+            disabled: false,
+        },
+        {
+            value: 'Пётр Дмитриевский',
+            label: 'Пётр Дмитриевский',
+            selected: false,
+            disabled: false,
+        },
+    ],
+    searchEnabled: false,
+    shouldSort: false,
+    placeholder: false,
 });
 
 const playButtons = document.querySelectorAll('.header__btn--play')
 playButtons.forEach(item => {
-  item.addEventListener('click', e => {
-    item.classList.toggle('header__btn--paused')
-  })
+    item.addEventListener('click', e => {
+        item.classList.toggle('header__btn--paused')
+    })
 })
 
 const accordion = new Accordion('.guests__accordion', {
-  duration: 300,
-  elementClass: 'guests__accordion-item',
-  panelClass: 'guests__items',
-  triggerClass: 'guests__accordion-item-title',
-  openOnInit: [0],
-  beforeOpen: (currentElement) => {
-    const plusButton = currentElement.querySelector('.arrow-in-circle')
-    plusButton.classList.add('arrow-in-circle--active')
-  },
-  beforeClose: (currentElement) => {
-    const plusButton = currentElement.querySelector('.arrow-in-circle')
-    plusButton.classList.remove('arrow-in-circle--active')
-  }
+    duration: 300,
+    elementClass: 'guests__accordion-item',
+    panelClass: 'guests__items',
+    triggerClass: 'guests__accordion-item-title',
+    openOnInit: [0],
+    beforeOpen: (currentElement) => {
+        const plusButton = currentElement.querySelector('.arrow-in-circle')
+        plusButton.classList.add('arrow-in-circle--active')
+    },
+    beforeClose: (currentElement) => {
+        const plusButton = currentElement.querySelector('.arrow-in-circle')
+        plusButton.classList.remove('arrow-in-circle--active')
+    }
 })
+
+
+const validation = new JustValidate(
+        '.about-us__form',
+        {
+            errorLabelStyle: null,
+        },
+    )
+;
+
+validation
+    .addField('#name', [
+        {
+            rule: 'required',
+            errorMessage: 'Вы не ввели имя',
+        },
+
+        {
+            rule: 'customRegexp',
+            value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+            errorMessage: 'Ошибка',
+        },
+    ])
+    .addField('#email', [
+        {
+            rule: 'required',
+            errorMessage: 'Вы не ввели e-mail',
+        },
+        {
+            rule: 'email',
+            errorMessage: 'Ошибка',
+        },
+    ]);
